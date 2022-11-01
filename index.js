e985b2079729abd732aa0ce949e9acedf9bd0b51
Shane Gannon <gannons@cisco.com>
2022-11-01 11:40:46 +0000

    Remove bad require

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const os = require('os');
-const greetModule = require('./build/Release/cookie-info-manager.node')
 
 function notWindowsSoDoNothing(url) {
     return "";
@@ -8,25 +7,6 @@ function notWindowsSoDoNothing(url) {
 if (os.type() === 'Windows_NT') {
     const cookieInfoManager = require('bindings')('cookie-info-manager');
     exports.getCookieInfoForUri = cookieInfoManager.getCookieInfoForUri;
-
-    const urls = [
-        "Upper Case",
-        "lower case",
-        "Tim",
-        "tim",
-        "https://www.google.com/",
-        "login.microsoftonline.com",
-        "https://login.microsoftonline.com/2a789914-be8c-49c8-9f76-a78c776ba89d/saml2",
-        "https://login.microsoftonline.com/common/GetCredentialType?mkt=en-US",
-        "https://login.microsoftonline.com/2a789914-be8c-49c8-9f76-a78c776ba89d/login"
-    ]
-
-    for (var url of urls) {
-        const cookies = cookieInfoManager.getCookieInfoForUri("https://login.microsoftonline.com/2a789914-be8c-49c8-9f76-a78c776ba89d/saml2")
-        for (const cookie of cookies) {
-            console.log(`cookieInfoManager.getCookieInfoForUri("${url}") : ${cookie.name} - ${cookie.data}\n`)
-        }
-    }
 }
 else {
     exports.getCookieInfoForUri = notWindowsSoDoNothing;
